032cc2a7e122308da4c0058288507c2a7630cb35
David Cantrell <david@cantrell.org.uk>
2013-01-09 16:42:01 +0000

    Import of DCANTRELL/Class-Mockable-1.1 from CPAN.
    gitpan-cpan-distribution: Class-Mockable
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         DCANTRELL/Class-Mockable-1.1.tar.gz
    gitpan-cpan-author:       DCANTRELL
    gitpan-cpan-maturity:     released

--- lib/Class/Mockable.pm
+++ lib/Class/Mockable.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 no strict 'refs';
 
-our $VERSION = '1.0002';
+our $VERSION = '1.1';
 
 our %mocks;
 
@@ -14,7 +14,16 @@ sub import {
 
     my $caller = (caller())[0];
 
-    foreach my $mock (keys %args) {
+    MOCK: foreach my $mock (keys %args) {
+
+        # For the special mock key 'methods', add mockability to the
+        # methods defined.
+        if (lc $mock eq 'methods') {
+            _add_method_mocking($caller, $args{$mock});
+            next MOCK;
+        }
+
+        # And add mocking for classes.
         my $singleton_name = "${caller}::$mock";
         $mocks{$singleton_name} = $args{$mock};
         *{"${caller}::_reset$mock"} = sub {
@@ -24,6 +33,38 @@ sub import {
             shift;
             if(exists($_[0])) { $mocks{$singleton_name} = shift; }
             $mocks{$singleton_name}
+        };
+    }
+}
+
+# Method mocking is slightly different, in that we need to create a setter, so
+# that the method can be replaced with a method mocker test interface or
+# code ref to do something else, as well as setting up the actual mock method
+# accessor to be used.
+
+sub _add_method_mocking {
+    my $caller       = shift;
+    my $method_mocks = shift;
+
+    for my $wrapper (keys %{$method_mocks}) {
+        my $singleton_name = "${caller}::$wrapper";
+        my $wrapped = $method_mocks->{$wrapper};
+
+        # I just invented the create-and-execute-subroutine operator :-)
+        &{*{"${caller}::_reset$wrapper"} = sub {
+            no warnings 'redefine';
+            *{"${caller}::$wrapper"} = sub {
+                my $invocant = shift;
+                $invocant->$wrapped(@_);
+            };
+        }};
+
+        *{"${caller}::_set$wrapper"} = sub {
+            my $invocant = shift;
+            if (exists($_[0])) {
+                no warnings 'redefine';
+                *{"${caller}::$wrapper"} = shift;
+            }
         };
     }
 }
@@ -61,7 +102,7 @@ is equivalent to:
             if (exists($_[0])) { $email_sender = shift; }
             return $email_sender;
         }
-    
+
         my $email_sent_storage;
         _reset_email_sent_storage();
         sub _reset_email_sent_storage {
@@ -97,9 +138,66 @@ you want to do an end-to-end test before releasing - you would do this:
 
 to restore the default functionality.
 
+
+=head2 METHOD MOCKING
+
+We also provide a way of easily inserting shims that wrap around methods
+that you have defined.
+
+    use Class::Mockable
+        methods => {
+            _foo => 'foo',
+        };
+
+The above will create a _foo sub on your class that by default will call your
+class's foo() subroutine.  This behaviour can be changed by calling the setter
+function _set_foo (where _foo is your identifier).  The default behaviour can be
+restored by calling _reset_foo (again, where _foo is your identifier).
+
+For example:
+
+    package Some::Module;
+
+    use Class::Mockable
+        methods => {
+            _bar => 'bar',
+        };
+
+    sub bar {
+        my $self = shift;
+        return "Bar";
+    }
+
+    sub foo {
+        my $self = shift;
+        return $self->_bar();
+    }
+
+    package main;
+
+    Some::Module->_set_bar(
+        sub {
+            my $self = shift;
+            return "Foo";
+        }
+    );
+
+    print Some::Module->bar();         # Prints "Bar"
+    print Some::Module->foo();         # Prints "Foo"
+
+    Some::Module->_reset_bar();
+
+    print Some::Module->bar();         # Prints "Bar"
+    print Some::Module->foo();         # Prints "Bar"
+
+It will also work for inserting a shim into a subclass to wrap around a method
+inherited from a superclass.
+
 =head1 AUTHOR
 
-Copyright 2012 UK2 Ltd and David Cantrell E<lt>david@cantrell.org.ukE<gt>
+Copyright 2012-13 UK2 Ltd and David Cantrell E<lt>david@cantrell.org.ukE<gt>
+
+With some bits from James Ronan
 
 This software is free-as-in-speech software, and may be used, distributed,
 and modified under the terms of either the GNU General Public Licence
